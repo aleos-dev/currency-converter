74f123292fc952af8c3cd2bb98e1a47892f68128
Aleksei Os. <oal.aleos@gmail.com>
2024-06-21 17:48:55 +0300

    Create SQLiteExceptionResolver to help with specific codes

--- src/main/java/com/aleos/util/SQLiteExceptionResolver.java
+++ src/main/java/com/aleos/util/SQLiteExceptionResolver.java
@@ -1,5 +1,6 @@
 package com.aleos.util;
 
+import com.aleos.exceptions.daos.ParseResultSetException;
 import com.aleos.exceptions.daos.DaoOperationException;
 import com.aleos.exceptions.daos.UniqueConstraintViolationException;
 
@@ -7,32 +8,40 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public final class SQLiteExceptionResolver {
 
-    private static final Map<String, Supplier<DaoOperationException>> registerMap = new HashMap<>();
+    private static final Map<String, DaoOperationException> registerMap = new HashMap<>();
 
     static {
         registerMap.put("SQLITE_CONSTRAINT_PRIMARYKEY",
-                () -> new UniqueConstraintViolationException("Primary key constraint violation"));
+                new UniqueConstraintViolationException("Primary key constraint violation"));
 
         registerMap.put("SQLITE_CONSTRAINT_UNIQUE",
-                () -> new UniqueConstraintViolationException("Unique constraint violation"));
+                new UniqueConstraintViolationException("Unique constraint violation"));
 
         registerMap.put("SQLITE_CONSTRAINT_FOREIGNKEY",
-                () -> new UniqueConstraintViolationException("Foreign key constraint violation"));
+                new UniqueConstraintViolationException("Foreign key constraint violation"));
 
         registerMap.put("SQLITE_CONSTRAINT_CHECK",
-                () -> new UniqueConstraintViolationException("Check constraint violation"));
+                new UniqueConstraintViolationException("Check constraint violation"));
 
         registerMap.put("SQLITE_CONSTRAINT_NOTNULL",
-                () -> new UniqueConstraintViolationException("Not null constraint violation"));
+                new UniqueConstraintViolationException("Not null constraint violation"));
+
+        registerMap.put("S1009", new ParseResultSetException("Invalid column index"));
+
+        registerMap.put("S0022", new ParseResultSetException("Invalid column name"));
     }
 
     private SQLiteExceptionResolver() {}
 
     public static Optional<DaoOperationException> getRegisteredException(SQLException e) {
-        return Optional.ofNullable(registerMap.get(e.getSQLState()).get());
+        return Optional.ofNullable(registerMap.get(e.getSQLState()));
+    }
+
+    public static DaoOperationException wrapException(SQLException e, String message) {
+        return getRegisteredException(e)
+                .orElse(new DaoOperationException(message, e));
     }
 }
