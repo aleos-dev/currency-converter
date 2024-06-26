ee411ee0165a448aef2f0427a0af680ba587cbe0
Aleksei Os. <oal.aleos@gmail.com>
2024-06-26 13:49:31 +0300

    Add ConversionRateMapper and CurrencyMapper

---
+++ src/main/java/com/aleos/mappers/CurrencyMapper.java
@@ -0,0 +1,59 @@
+package com.aleos.mappers;
+
+import com.aleos.models.dtos.CurrencyPayload;
+import com.aleos.models.dtos.CurrencyResponse;
+import com.aleos.models.entities.Currency;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+
+public class CurrencyMapper {
+
+    private final ModelMapper modelMapper;
+
+    public CurrencyMapper(ModelMapper modelMapper) {
+
+        this.modelMapper = modelMapper;
+        configureMappings();
+    }
+
+    private void configureMappings() {
+
+       Converter<Currency, CurrencyResponse> toDtoConverter = context -> {
+           Currency source = context.getSource();
+
+           return new CurrencyResponse(
+                   source.getId(),
+                   source.getFullname(),
+                   source.getCode(),
+                   source.getSign()
+           );
+       };
+
+       Converter<CurrencyPayload, Currency>  toEntityConverter = context -> {
+
+            CurrencyPayload source = context.getSource();
+
+           var currency = new Currency();
+           currency.setFullname(source.name());
+           currency.setCode(source.code());
+           currency.setSign(source.sign());
+
+           return currency;
+       };
+
+        TypeMap<Currency, CurrencyResponse> toDtoTypeMap = modelMapper.createTypeMap(Currency.class, CurrencyResponse.class);
+        toDtoTypeMap.setConverter(toDtoConverter);
+
+        TypeMap<CurrencyPayload, Currency> toEntityTypeMap = modelMapper.createTypeMap(CurrencyPayload.class, Currency.class);
+        toEntityTypeMap.setConverter(toEntityConverter);
+    }
+
+    public CurrencyResponse toDto(Currency currency) {
+        return modelMapper.map(currency, CurrencyResponse.class);
+    }
+
+    public Currency toEntity(CurrencyPayload currencyPayload) {
+        return modelMapper.map(currencyPayload, Currency.class);
+    }
+}
