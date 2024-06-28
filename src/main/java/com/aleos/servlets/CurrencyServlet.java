bef6d813e76f1a38d39db0e1926cec95332d0b66
Aleksei Os. <oal.aleos@gmail.com>
2024-06-28 21:36:40 +0300

    ref: change response approach, instead of using ResponseWrapper set response object as a context attribute

--- src/main/java/com/aleos/servlets/CurrencyServlet.java
+++ src/main/java/com/aleos/servlets/CurrencyServlet.java
@@ -1,9 +1,10 @@
 package com.aleos.servlets;
 
-import com.aleos.models.dtos.CurrencyCodePayload;
-import com.aleos.models.dtos.ConversionRateResponse;
+import com.aleos.models.dtos.CurrencyIdentifierPayload;
+import com.aleos.models.dtos.CurrencyResponse;
 import com.aleos.models.dtos.ErrorResponse;
 import com.aleos.services.CurrencyService;
+import com.aleos.util.AttributeNameUtil;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,29 +14,38 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
 
+import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
+import static jakarta.servlet.http.HttpServletResponse.SC_OK;
+
 @WebServlet("/currency/*")
 public class CurrencyServlet extends HttpServlet {
 
     private transient CurrencyService currencyService;
 
+
     @Override
     public void init(ServletConfig config) throws ServletException {
+
         super.init(config);
-        currencyService = (CurrencyService) config.getServletContext().getAttribute("currencyService");
+        currencyService = (CurrencyService) config.getServletContext()
+                .getAttribute(AttributeNameUtil.getName(CurrencyService.class));
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-        var currencyIdentifierPayload = (CurrencyCodePayload) request.getAttribute("currencyIdentifierPayload");
+        var model = (CurrencyIdentifierPayload) request.getAttribute(AttributeNameUtil.PAYLOAD_MODEL_ATTR);
 
-        Optional<ConversionRateResponse> currency = currencyService.findByIdentifier(currencyIdentifierPayload.code());
+        Optional<CurrencyResponse> currency = currencyService.findByIdentifier(model);
 
-        ResponseWrapper responseWrapper = (ResponseWrapper) response;
         if (currency.isPresent()) {
-            responseWrapper.composeResponse(HttpServletResponse.SC_OK, currency.get());
-        } else {
-            responseWrapper.composeResponse(HttpServletResponse.SC_NOT_FOUND, new ErrorResponse("Currency not found"));
+            request.setAttribute(AttributeNameUtil.RESPONSE_MODEL_ATTR, currency.get());
+            response.setStatus(SC_OK);
+            return;
         }
+
+        request.setAttribute(AttributeNameUtil.RESPONSE_MODEL_ATTR,
+                new ErrorResponse("Currency with identifier: %s, does not exist.".formatted(model.identifier())));
+        response.setStatus(SC_NOT_FOUND);
     }
 }
