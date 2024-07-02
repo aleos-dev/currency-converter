e60e518318267b1b19e35d454653edb95233a4f6
Aleksei Os. <oal.aleos@gmail.com>
2024-07-02 12:30:03 +0300

    ref: add some records to be able to test a crossrate

--- src/test/resources/db/test_migration/V301__Insert_Test_Init_Data.sql
+++ src/test/resources/db/test_migration/V301__Insert_Test_Init_Data.sql
@@ -9,13 +9,15 @@ INSERT INTO currencies (code, fullname, sign) VALUES ('CNY', 'Chinese Yuan', '¥
 INSERT INTO currencies (code, fullname, sign) VALUES ('RUB', 'Russian Ruble', '₽');
 INSERT INTO currencies (code, fullname, sign) VALUES ('UAH', 'Ukrainian Hryvnia', '₴');
 
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 2, 0.85); -- USD to EUR
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 3, 110.15); -- USD to JPY
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 4, 0.75); -- USD to GBP
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 5, 1.35); -- USD to AUD
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 6, 1.25); -- USD to CAD
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 7, 0.92); -- USD to CHF
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 8, 6.45); -- USD to CNY
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 9, 74.25); -- USD to RUB
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (1, 10, 27.90); -- USD to UAH
-INSERT INTO conversion_rates (base_currency, target_currency, rate) VALUES (2, 3, 129.53); -- EUR to JPY
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 2, 0.93); -- USD to EUR
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 3, 161.61); -- USD to JPY
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 4, 0.79); -- USD to GBP
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 5, 1.35); -- USD to AUD
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 6, 1.25); -- USD to CAD
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 7, 0.92); -- USD to CHF
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 8, 7.27); -- USD to CNY
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 9, 86.75); -- USD to RUB
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (1, 10, 40.9); -- USD to UAH
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (2, 3, 173.32); -- EUR to JPY
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (3, 8 , 0.045); -- JPY to CNY
+INSERT INTO conversion_rates (base_currency_id, target_currency_id, rate) VALUES (10, 8 , 0.18); -- UAH to CNY
