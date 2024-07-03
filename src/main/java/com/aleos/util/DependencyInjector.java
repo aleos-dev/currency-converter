991d6c889d62819763b7cfdc670cda30fec61cda
Aleksei Os. <oal.aleos@gmail.com>
2024-07-03 17:40:09 +0300

    feat: create DependencyInjector util class

---
+++ src/main/java/com/aleos/util/DependencyInjector.java
@@ -0,0 +1,32 @@
+package com.aleos.util;
+
+import jakarta.servlet.ServletContext;
+
+import java.lang.reflect.Field;
+
+public final class DependencyInjector {
+
+    private DependencyInjector() {
+        throw new UnsupportedOperationException("DependencyInjector can't be instantiated.");
+    }
+
+    public static void inject(Object target, ServletContext context) {
+
+        for (Field field : target.getClass().getDeclaredFields()) {
+
+            Class<?> clazz = field.getType();
+
+            Object attribute = context.getAttribute(AttributeNameUtil.getName(clazz));
+
+            if (attribute != null) {
+                field.setAccessible(true);
+                try {
+                    field.set(target, attribute);
+                } catch (IllegalAccessException e) {
+                    throw new ExceptionInInitializerError("Can't inject %s to class variable of %s"
+                            .formatted(clazz.getSimpleName(), target.getClass().getSimpleName()));
+                }
+            }
+        }
+    }
+}
