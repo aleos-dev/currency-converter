b404888e5da84fb568cab01c6efffbd65bcfdcb5
Aleksei Os. <oal.aleos@gmail.com>
2024-07-04 20:55:11 +0300

    feat: add EncodingFilter

---
+++ src/main/java/com/aleos/servlets/filters/EncodingFilter.java
@@ -0,0 +1,19 @@
+package com.aleos.servlets.filters;
+
+import com.aleos.util.PropertiesUtil;
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        String charset = PropertiesUtil.getProperty("response.charset");
+        req.setCharacterEncoding(charset);
+        resp.setCharacterEncoding(charset);
+
+        chain.doFilter(req, resp);
+    }
+}
