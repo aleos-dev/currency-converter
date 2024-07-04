17a6cee0edf8a2a91ada0249826fdb669ca8eda9
Aleksei Os. <oal.aleos@gmail.com>
2024-07-04 20:55:58 +0300

    ref: refactor ResponseFilter, extract cors and encoding headers to separate filters

---
+++ src/main/java/com/aleos/servlets/filters/JsonResponseFilter.java
@@ -0,0 +1,52 @@
+package com.aleos.servlets.filters;
+
+import com.aleos.exceptions.servlets.HttpResponseWritingException;
+import com.aleos.exceptions.servlets.WrappedJsonProcessingException;
+import com.aleos.util.AttributeNameUtil;
+import com.aleos.util.PropertiesUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class JsonResponseFilter extends BaseFilter {
+
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        chain.doFilter(req, resp);
+
+        processResponse((HttpServletRequest) req, ((HttpServletResponse) resp));
+    }
+
+    private void processResponse(HttpServletRequest req, HttpServletResponse resp) {
+            Optional.ofNullable(req.getAttribute(AttributeNameUtil.RESPONSE_MODEL_ATTR))
+                    .map(this::toJson)
+                    .ifPresent(json -> write(json, resp));
+    }
+
+    private void write(String json, HttpServletResponse resp) {
+        try {
+            resp.getWriter().write(json);
+            resp.setContentType(PropertiesUtil.getProperty("response.contentType"));
+        } catch (IOException e) {
+            throw new HttpResponseWritingException("Error writing json response", e);
+        }
+    }
+
+    private String toJson(Object responseObject) {
+
+        try {
+            return objectMapper.writeValueAsString(responseObject);
+        } catch (JsonProcessingException e) {
+            throw new WrappedJsonProcessingException(
+                    "Cannot parse object to JSON format: %s".formatted(responseObject), e);
+        }
+    }
+}
