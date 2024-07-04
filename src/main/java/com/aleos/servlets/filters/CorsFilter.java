fde75dbd35ccd0c555f2b41a8fe5c6ea255849cb
Aleksei Os. <oal.aleos@gmail.com>
2024-07-04 20:54:54 +0300

    feat: add CorsFilter

---
+++ src/main/java/com/aleos/servlets/filters/CorsFilter.java
@@ -0,0 +1,19 @@
+package com.aleos.servlets.filters;
+
+import com.aleos.util.PropertiesUtil;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        String allowOrigin = PropertiesUtil.getProperty("response.cors.allowOrigin");
+        ((HttpServletResponse) resp).setHeader("Access-Control-Allow-Origin", allowOrigin);
+
+        chain.doFilter(req, resp);
+    }
+}
