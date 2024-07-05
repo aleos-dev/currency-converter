ca0884454aa8335015b35a20cdee291b9439baf7
Aleksei Os. <oal.aleos@gmail.com>
2024-07-05 21:26:35 +0300

    rec: change the data source initialization based on properties

--- src/main/java/com/aleos/util/DbUtil.java
+++ src/main/java/com/aleos/util/DbUtil.java
@@ -5,7 +5,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.util.Properties;
 
 public final class DbUtil {
@@ -13,10 +12,11 @@ public final class DbUtil {
     private static final DataSource DATA_SOURCE;
 
     private DbUtil() {
+        throw new UnsupportedOperationException("DbUtil instance cannot be created.");
     }
 
     static {
-        DATA_SOURCE = initDataSource("database.properties");
+        DATA_SOURCE = initDataSource();
         runFlywayMigration(DATA_SOURCE);
     }
 
@@ -26,23 +26,23 @@ public final class DbUtil {
 
     public static DataSource getTestDataSource() {
 
-        var dataSource = initDataSource("test-database.properties");
+        var dataSource = initDataSource();
         runFlywayMigration(dataSource);
         return dataSource;
     }
 
-    private static DataSource initDataSource(String resourceName) {
+    private static DataSource initDataSource() {
 
-        try {
             Properties properties = new Properties();
-            properties.load(DbUtil.class.getClassLoader().getResourceAsStream(resourceName));
+            properties.setProperty("jdbcUrl", PropertiesUtil.DATABASE_URL);
+            properties.setProperty("username", PropertiesUtil.DATABASE_USER);
+            properties.setProperty("password", PropertiesUtil.DATABASE_PASSWORD);
+            properties.setProperty("driverClassName", PropertiesUtil.DATABASE_DRIVER);
+            properties.setProperty("maximumPoolSize", PropertiesUtil.DATABASE_POOL_SIZE);
 
             HikariConfig hikariConfig = new HikariConfig(properties);
 
             return new HikariDataSource(hikariConfig);
-        } catch (IOException e) {
-            throw new ExceptionInInitializerError("Failed to initialize DataSource" + e.getMessage());
-        }
     }
 
     private static void runFlywayMigration(DataSource dataSource) {
