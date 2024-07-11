6d5ea4aec607a4967307c471a0e600cbe805c5fd
Aleksei Os. <oal.aleos@gmail.com>
2024-07-11 18:38:07 +0300

    test: add test for common filters

---
+++ src/test/java/com/aleos/filters/common/JsonResponseFilterTest.java
@@ -0,0 +1,79 @@
+package com.aleos.filters.common;
+
+import com.aleos.util.AttributeNameUtil;
+import com.aleos.util.PropertiesUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.mockito.Mockito.*;
+
+class JsonResponseFilterTest {
+
+    @InjectMocks
+    JsonResponseFilter jsonResponseFilter;
+
+    @Mock
+    ObjectMapper objectMapper;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    HttpServletResponse response;
+
+    @Mock
+    FilterChain chain;
+
+    private AutoCloseable mocks;
+
+    @BeforeEach
+    void setUp() {
+        mocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        if (mocks != null) {
+            mocks.close();
+        }
+    }
+
+    @Test
+    void doFilter_WriteResponseInJson_IfPresent() throws ServletException, IOException {
+        final var stub = new Object();
+        final var json = "{}";
+        final var printWriter = mock(PrintWriter.class);
+        doReturn(stub).when(request).getAttribute(AttributeNameUtil.RESPONSE_MODEL_ATTR);
+        doReturn(json).when(objectMapper).writeValueAsString(stub);
+        doReturn(printWriter).when(response).getWriter();
+
+        jsonResponseFilter.doFilter(request, response, chain);
+
+        verify(objectMapper).writeValueAsString(stub);
+        verify(response).getWriter();
+        verify(response).setContentType(PropertiesUtil.getProperty("response.contentType"));
+    }
+
+    @Test
+    void doFilter_DoNothing_IfResponseObjectIsNotPresent() throws ServletException, IOException {
+        doReturn(null).when(request).getAttribute(AttributeNameUtil.RESPONSE_MODEL_ATTR);
+
+        jsonResponseFilter.doFilter(request, response, chain);
+
+        verify(objectMapper, never()).writeValueAsString(any());
+        verify(response, never()).getWriter();
+        verify(response, never()).setContentType(anyString());
+    }
+}
