422ec29395c0a22889bfa9899944ad40d84e8ee5
Aleksei Os. <oal.aleos@gmail.com>
2024-07-13 14:19:11 +0300

    fix: add super constructor

--- src/main/java/com/aleos/exception/dao/UnknownParameterTypeException.java
+++ src/main/java/com/aleos/exception/dao/UnknownParameterTypeException.java
@@ -3,5 +3,6 @@ package com.aleos.exception.dao;
 public class UnknownParameterTypeException extends RuntimeException {
 
     public UnknownParameterTypeException(String s) {
+        super(s);
     }
 }
