6972a1e7e943cc647a44fca98919b6a3a82efcea
Aleksei Os. <oal.aleos@gmail.com>
2024-07-19 20:34:04 +0300

    ref: extract not found response to BaseServlet

--- backend/src/main/java/com/aleos/servlet/BaseServlet.java
+++ backend/src/main/java/com/aleos/servlet/BaseServlet.java
@@ -1,9 +1,14 @@
 package com.aleos.servlet;
 
 import com.aleos.util.ComponentInitializerUtil;
+import com.aleos.util.RequestAttributeUtil;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
 public abstract class BaseServlet extends HttpServlet {
 
@@ -11,5 +16,11 @@ public abstract class BaseServlet extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         ComponentInitializerUtil.injectDependencies(config.getServletContext(), this);
+    }
+
+
+    protected void setNotFoundResponse(HttpServletRequest req, HttpServletResponse resp, String message) {
+        RequestAttributeUtil.setResponse(req, message);
+        resp.setStatus(SC_NOT_FOUND);
     }
 }
--- backend/src/main/java/com/aleos/servlet/ConversionServlet.java
+++ backend/src/main/java/com/aleos/servlet/ConversionServlet.java
@@ -2,15 +2,12 @@ package com.aleos.servlet;
 
 import com.aleos.model.dto.in.ConversionPayload;
 import com.aleos.model.dto.out.ConversionResponse;
-import com.aleos.model.dto.out.Error;
 import com.aleos.service.ConversionService;
 import com.aleos.util.RequestAttributeUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
-
-import static jakarta.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
 public class ConversionServlet extends BaseServlet {
 
@@ -24,10 +21,6 @@ public class ConversionServlet extends BaseServlet {
 
         byPayload.ifPresentOrElse(
                 responseModel -> RequestAttributeUtil.setResponse(req, responseModel),
-                () -> {
-                    RequestAttributeUtil.setResponse(req, Error.of("Conversion is not possible."));
-                    resp.setStatus(SC_NOT_FOUND);
-                }
-        );
+                () -> setNotFoundResponse(req, resp, "Conversion is not found and can't be calculated."));
     }
 }
