b60922881dada7d95a7489a700d7d13a8200a72a
Aleksei Os. <oal.aleos@gmail.com>
2024-07-19 20:37:38 +0300

    fix: the cache invalidation works for patch methods

--- backend/src/main/java/com/aleos/service/CacheService.java
+++ backend/src/main/java/com/aleos/service/CacheService.java
@@ -24,6 +24,12 @@ public class CacheService {
     }
 
     public void remove(String cacheKey) {
-        cache.remove(cacheKey);
+        if (cacheKey.contains("currency")) {
+            cache.entrySet().removeIf(entry -> entry.getKey().matches(".*(currency|currencies).*"));
+        } else if (cacheKey.contains("exchangeRate")) {
+            cache.entrySet().removeIf(entry -> entry.getKey().matches(".*(exchangeRate|exchangeRates).*"));
+        } else {
+            cache.remove(cacheKey);
+        }
     }
 }
